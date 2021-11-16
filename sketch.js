5cc2fcf3e2f26155067425efb30a6352465d9002
shaynehuang98 <59852035+shaynehuang98@users.noreply.github.com>
2021-11-16 11:30:35 -0800

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,11 @@
+const canvasWidth = window.innerWidth
+const canvasHeight = window.innerHeight
+
+function setup (){
+    createCanvas(canvasWidth, canvasHeight)
+}
+
+function draw(){
+    background(30)
+}
+
